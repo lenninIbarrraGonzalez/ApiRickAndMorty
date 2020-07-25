2cda5eb090fe593bf11d2e4e208a3bc3bf5b4e58
Lennin666 <soporte@allnexus.com.co>
2020-07-25 16:34:08 -0500

    implementando el loading y el butoon de read more

--- src/App.js
+++ src/App.js
@@ -3,14 +3,17 @@ import logo from './logo.png';
 import './App.css';
 // import data from './data.json';
 import Card from '../src/components/Card';
+import Loading from '../src/components/Loading';
 
 
 class App extends React.Component {
   state = {
+    loading: true,
     data: {
-      info:[],
+      info: [],
       results: [],
     },
+    nextPage: 1,
   };
 
   componentDidMount() {
@@ -18,18 +21,38 @@ class App extends React.Component {
   }
 
   fetchCharacters = async () => {
-    const response = await fetch("https://rickandmortyapi.com/api/character/");
-    const data = await response.json();
-    // console.log(data);
     this.setState({
-      data: {
-        info: data.info,
-        results: data.results,
-      }
+      loading: true,
+      error: null,
     });
+
+    try {
+      const response = await fetch(
+        `https://rickandmortyapi.com/api/character/?page=${this.state.nextPage}`
+      );
+      const data = await response.json();
+      // console.log(data);
+      this.setState({
+        loading: false,
+        data: {
+          info: data.info,
+          results: [].concat(this.state.data.results, data.results),
+        },
+        nextPage: this.state.nextPage + 1,
+      });
+    } catch (error) {
+      this.setState({
+        loading: false,
+        error: error,
+      });
+    }
   };
-  
+
   render() {
+    if(this.state.error){
+      return `Error: ${this.state.error.message}`;
+    }
+
     return (
       <div className="container">
         <div className="App">
@@ -39,10 +62,18 @@ class App extends React.Component {
             {console.log(this.state.data)}
             {this.state.data.results.map((character) => (
               <li key={character.id}>
-                <Card character={character}/>
+                <Card character={character} />
               </li>
             ))}
           </ul>
+          {this.state.loading && <Loading />}
+          <button
+            type="button"
+            className="btn btn-primary btn-lg btn-block"
+            onClick={() => this.fetchCharacters()}
+          >
+            Cargar más personajes
+          </button>
         </div>
       </div>
     );
---
+++ src/components/Loading.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import '../index.css';
+function Loading () {
+  return (
+    <div className="loading">
+      <div className="lds-default">
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+      </div>
+    </div>
+  );
+}
+
+export default Loading;
